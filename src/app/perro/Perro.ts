04431bf6efa4b8ea81819d02f1622756ccafe014
Almanza33 <d.almanza@uniandes.edu.co>
2022-11-28 13:56:32 -0500

    Hu 7 y 8 terminadas

--- src/app/perro/Perro.ts
+++ src/app/perro/Perro.ts
@@ -4,13 +4,13 @@ export class Perro {
   id: number;
   nombre: string;
   fotoAntes: string;
-  fotodespues: string;
+  fotoDespues: string;
   hallOfFame: HallOfFame;
-  public constructor(id: number, nombre:string, fotoAntes:string, fotodespues:string, hallOfFame: HallOfFame){
+  public constructor(id: number, nombre:string, fotoAntes:string, fotoDespues:string, hallOfFame: HallOfFame){
     this.id = id;
     this.nombre = nombre;
     this.fotoAntes = fotoAntes;
-    this.fotodespues = fotodespues;
+    this.fotoDespues = fotoDespues;
     this.hallOfFame = hallOfFame;
   }
 
