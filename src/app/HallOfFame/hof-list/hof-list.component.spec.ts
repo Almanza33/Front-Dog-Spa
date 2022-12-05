fe4cda11ab832e443af727087de17604835d4499
Almanza33 <d.almanza@uniandes.edu.co>
2022-12-04 20:27:44 -0500

    tests HU 7 y 8

--- src/app/HallOfFame/hof-list/hof-list.component.spec.ts
+++ src/app/HallOfFame/hof-list/hof-list.component.spec.ts
@@ -4,13 +4,22 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { HofListComponent } from './hof-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Sedes } from 'src/app/sedes/Sedes';
+import { faker } from '@faker-js/faker';
+import { HallOfFame } from '../HallOfFame';
+import { HallOfFameDetail } from '../HallOfFame-detail';
+import { FormsModule } from '@angular/forms';
 
 describe('HofListComponent', () => {
   let component: HofListComponent;
   let fixture: ComponentFixture<HofListComponent>;
+  let debug:DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[HttpClientModule, RouterTestingModule, FormsModule],
       declarations: [ HofListComponent ]
     })
     .compileComponents();
@@ -19,7 +28,51 @@ describe('HofListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HofListComponent);
     component = fixture.componentInstance;
+
+
+    let sede = new Sedes(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence()
+    )
+
+    let testHofs: Array<HallOfFameDetail> = [];
+
+    for(let i = 0; i < 10 ; i++){
+      testHofs[i] = new HallOfFameDetail(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        sede,
+        []
+      )
+    }
+
+
+    component.hofs = testHofs;
+
+
+
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a h2 title', () => {
+    const element: HTMLElement = debug.query(By.css('h2.text-center.display-4')).nativeElement;
+    expect(element.textContent).toContain('Halls Of Fame');
+  });
+
+  it('Should have 10 div.book-card elements', ()=> {
+    expect(debug.queryAll(By.css('div.book-card')).length == 10).toBeTrue();
+  });
+
+
 
 });
--- src/app/HallOfFame/perros-list/perros-list.component.spec.ts
+++ src/app/HallOfFame/perros-list/perros-list.component.spec.ts
@@ -2,15 +2,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { PerrosListComponent } from './perros-list.component';
+import { Sedes } from 'src/app/sedes/Sedes';
+import { HallOfFameDetail } from '../HallOfFame-detail';
+import { HallOfFame } from "../HallOfFame";
+import { Perro } from 'src/app/perro/Perro';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PerrosListComponent', () => {
   let component: PerrosListComponent;
   let fixture: ComponentFixture<PerrosListComponent>;
+  let debug: DebugElement;
+  let perrosSize: number;
+  let hoFId:number;
+  let hoFNombre:string;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports:[HttpClientModule, RouterTestingModule],
       declarations: [ PerrosListComponent ]
     })
     .compileComponents();
@@ -19,7 +31,62 @@ describe('PerrosListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PerrosListComponent);
     component = fixture.componentInstance;
+    perrosSize = 6;
+
+    hoFId = faker.datatype.number()
+    hoFNombre = faker.lorem.sentence()
+
+
+    let sede = new Sedes(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence()
+    )
+
+    let hoF = new HallOfFame(
+      hoFId,
+      hoFNombre,
+      sede
+    )
+
+    let perros = Array(perrosSize);
+    for (let i = 0; i < perrosSize ; i++){
+      perros[i] = new Perro(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.image.imageUrl(),
+        faker.image.imageUrl(),
+        hoF
+      )
+    }
+
+    component.hallOfFameDetail = new HallOfFameDetail(
+      hoFId,
+      hoFNombre,
+      sede,
+      perros
+
+    )
+
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a p.h7.p-2 element with HallOfFame.name', () =>{
+    const element: HTMLElement = debug.query(By.css('p.h7.p-2.text-center.display-4.titulo')).nativeElement;
+    expect(element.textContent).toContain(component.hallOfFameDetail.nombre);
+  });
+
+
+
+
 
 });
