852c5315943f764abcc89b5b926103682a1dcdb0
camilo morillo <c.morillo@uniandes.edu.co>
2022-12-06 14:34:44 -0500

    stetic changes

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  goToProductos() {
+    this.router.navigate(['/', 'producto']);
+
+  }
+
+  goToSedes() {
+    this.router.navigate(['/', 'sedes']);
+  }
+
+  goToPacks() {
+    this.router.navigate(['/', 'packs'])
+  }
+
+  goToHalls() {
+    this.router.navigate(['/', 'halls'])
+  }
 }
